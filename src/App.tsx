89dbcf365c7c9756be7dd6ade65d247f239970c3
Skye Brown <skye.brownh@gmail.com>
2026-01-29 05:45:03 -0500

    change react router from data mode to declarative mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import AssignmentModal from "@/components/ScheduleGrid/modals/AssignmentModal";
+import ScheduleGridEntry from "@/components/ScheduleGrid/ScheduleGridEntry";
+import ScheduleGridLayout from "@/components/ScheduleGrid/ScheduleGridLayout";
+import { Route, Routes } from "react-router";
+
 const App = () => {
-    return <></>;
+    return (
+        <Routes>
+            <Route path="/" element={<ScheduleGridEntry />} />
+            <Route path="schedules/:scheduleId/grid" element={<ScheduleGridLayout />}>
+                <Route path="events/:eventId/assignments/:assignmentId" element={<AssignmentModal />} />
+            </Route>
+        </Routes>
+    );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
 import { queryClient } from "@lib/queryClient.ts";
-import { router } from "@lib/router.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router/dom";
+import { BrowserRouter } from "react-router";
+import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     </StrictMode>
