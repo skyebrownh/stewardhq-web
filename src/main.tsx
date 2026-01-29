70fa316e0d1d9135ada73b1b389df831946ace58
Skye Brown <skye.brownh@gmail.com>
2026-01-29 06:08:51 -0500

    add clerk auth

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/react-router";
 import { queryClient } from "@lib/queryClient.ts";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -7,11 +8,19 @@ import { BrowserRouter } from "react-router";
 import App from "./App";
 import "./index.css";
 
+const PUBLISHABLE_KEY: string = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string;
+
+if (!PUBLISHABLE_KEY) {
+    throw new Error("Add your Clerk Publishable Key to the .env file");
+}
+
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
-                <App />
+                <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+                    <App />
+                </ClerkProvider>
             </BrowserRouter>
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
